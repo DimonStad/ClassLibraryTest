abd79273fc22189d00b2484b11366fb145ee3af5
Dmitriy <stadnit@mail.ru>
2021-10-19 13:14:56 +0300

    first commit

---
+++ ClassCounter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ClassLibraryTest
+{
+         class ClassCounter
+    {
+
+         private  Dictionary<string, int> Counter(string str) 
+        { 
+        var pattern = @"[^а-я|a-z]+";
+        var regex = new Regex(pattern, RegexOptions.IgnoreCase);
+
+
+
+        var arr = regex.Split(str.ToLower()).Where(e => e.Length >= 1);
+        Dictionary<string, int> statistics = arr.GroupBy(word => word).ToDictionary(
+            kvp => kvp.Key,
+            kvp => kvp.Count());
+
+        var ordered = statistics.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+        
+
+            return ordered;
+        }
+
+         
+
+    }
+
+}
